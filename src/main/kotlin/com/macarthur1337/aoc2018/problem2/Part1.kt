ad69b7a72a25196ac0c05a68669cfd8a78a91c9c
Neil E. Absil <neil@keystone-strategic.com>
2018-12-05 17:41:19 +0100

    *first work on part 1 (problem 2).

--- src/main/kotlin/com/macarthur1337/aoc2018/problem2/Part1.kt
+++ src/main/kotlin/com/macarthur1337/aoc2018/problem2/Part1.kt
@@ -1,16 +1,24 @@
 package com.macarthur1337.aoc2018.problem2
 
 import org.springframework.util.ResourceUtils
-import java.io.InputStream
 
 /**
  * @author N. E. Absil
  * @since 05-12-2018
  */
 fun main(args: Array<String>) {
-    val inputStream: InputStream = ResourceUtils.getFile("classpath:input1").inputStream()
     val lineList = mutableListOf<String>()
+    ResourceUtils.getFile("classpath:input2")
+            .inputStream()
+            .bufferedReader()
+            .useLines { lines -> lines.forEach { lineList.add(it) } }
 
-    inputStream.bufferedReader().useLines { lines -> lines.forEach { lineList.add(it) } }
+
+    val list = lineList.asSequence().iterator()
+
+    while (list.hasNext()) {
+        println(list.next().asSequence().groupingBy { it }.eachCount().filter { it.value > 1 })
+
+    }
 
 }
