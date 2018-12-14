4d9528f4ec6e065eab676713681bf74c75b8de3f
Neil E. Absil <neil@keystone-strategic.com>
2018-12-14 23:38:41 +0100

    *day5 part1 solution

--- src/main/kotlin/com/macarthur1337/aoc2018/day5/Part1.kt
+++ src/main/kotlin/com/macarthur1337/aoc2018/day5/Part1.kt
@@ -6,70 +6,36 @@ import org.springframework.util.ResourceUtils
  * @author N. E. Absil
  * @since 05-12-2018
  */
+data class Pair(val first: Char = ' ', val second: Char = ' ')
+
 fun main(args: Array<String>) {
     val lineList = mutableListOf<String>()
-    ResourceUtils.getFile("classpath:input")
+    ResourceUtils.getFile("classpath:input5")
         .inputStream()
         .bufferedReader()
         .useLines { lines -> lines.forEach { lineList.add(it) } }
 
     val line = lineList[0].asSequence().iterator()
-    val initial = mutableListOf<Char>()
+    val polymers = mutableListOf<Char>()
+    polymers.add(line.next())
 
-    while(line.hasNext()){
-        val current =  line.next()
-        if(line.hasNext()){
-            val next = line.next()
-            if (current.toLowerCase() == next.toLowerCase() &&
-                ((current.isLowerCase() && next.isUpperCase())
-                        || (current.isUpperCase() && next.isLowerCase()))) {
-               continue
+    loop@ while (line.hasNext()) {
+        val current = line.next()
+        when {
+            polymers.size > 0 && reducePolymer(current, polymers.last()) -> {
+                polymers.removeAt(polymers.size - 1)
+                continue@loop
             }
-            else{
-                var previous = ' '
-                if(initial.isNotEmpty()) { previous = initial.last() }
-                if (current.toLowerCase() == previous.toLowerCase() &&
-                    ((current.isLowerCase() && previous.isUpperCase())
-                            || (current.isUpperCase() && previous.isLowerCase()))) {
-                    initial.removeAt(initial.size-1)
-                    initial.add(next)
-                }
-                else {
-                    initial.add(current)
-                    initial.add(next)
-
-                }
-            }
+            else -> polymers.add(current)
         }
     }
-    val result = reduce(initial.joinToString(prefix = "", postfix = "", separator = ""))
-    println(result.length)
 
-    println("dabCBAcaDA")
-    println(result)
-    require(result == "dabCBAcaDA")
-
-   // val result = reduce(initial.joinToString(prefix = "", postfix = "", separator = ""))
-   /* val result = reduce(lineList[0])
-    println(result)
-    println(result.length)*/
+    println(polymers.size)
 }
 
-//recursive reduction function
-fun reduce(polymer: String): String {
-    println(polymer.length)
-    var reduced = polymer
-    var index = 0
-        while (index + 1 < polymer.length) {
-            var current = polymer[index]
-            var next = polymer[index+1]
-            if (current.toLowerCase() == next.toLowerCase() &&
-                ((current.isLowerCase() && next.isUpperCase())
-                        || (current.isUpperCase() && next.isLowerCase()))) {
-                 return reduce(polymer.removeRange(index, index+1))
-            }
-                index++
-        }
-
-    return reduced
+fun reducePolymer(first: Char, second: Char): Boolean {
+    if (first == second) return false
+    if (first.toUpperCase() == second) return true
+    if (first.toLowerCase() == second) return true
+    return false
 }
